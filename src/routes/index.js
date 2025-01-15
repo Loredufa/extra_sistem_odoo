c72c6bfb21c57d2c43c0af502f611fd4821ce34f
loredufa <lorenadufaur@gmail.com>
2025-01-15 16:47:47 -0300

    comento verify

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-const verifyToken = require('../utils/middlewares/verifyToken');
+//const verifyToken = require('../utils/middlewares/verifyToken');
 
 
 const productRoute = require('./product');
